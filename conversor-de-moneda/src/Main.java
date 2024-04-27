07378d846cd661204e3d9a9dbba39ff545dc8f2d
EduRiu <chino_926@hotmail.com>
2024-04-26 23:30:35 -0300

    Update Main.java
    se creo bucle

--- conversor-de-moneda/src/Main.java
+++ conversor-de-moneda/src/Main.java
@@ -3,33 +3,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        boolean salida = true;
         var moneda = new BuscarMoneda();
         var lista = new ListaDeMonedas();
         conexionApi conexion = new conexionApi();
 
         Scanner leer = new Scanner(System.in);
         System.out.println("BienVenido al conversor de moneda");
-        System.out.println("Disonde de " +  lista.disponibles().size() + " monedas diferentes para la conversion");
-        System.out.println("ingrese cantidad de dinero a convertir");
-        int cantidad = leer.nextInt();
-        System.out.println("Paises disponibles");
-        lista.listar();
+        System.out.println("Disponde de " + lista.disponibles().size() + " monedas diferentes para la conversion");
+        do {
 
-        System.out.println("de que pais es la moneda?");
-        String inicial = lista.nombreYmonedas(leer.next());
+            System.out.println("ingrese cantidad de dinero a convertir");
+            int cantidad = leer.nextInt();
+            System.out.println("Paises disponibles");
+            lista.listar();
 
-        System.out.println("a la moneda de que pais quiere convertir?");
-        String destino = lista.nombreYmonedas(leer.next());
+            System.out.println("de que pais es la moneda?");
+            String inicial = lista.nombreYmonedas(leer.next());
 
-        modeloMoneda resultado = conexion.cotizar(inicial, destino, cantidad);
+            System.out.println("a la moneda de que pais quiere convertir?");
+            String destino = lista.nombreYmonedas(leer.next());
 
-        NombreMoneda tipo = conexion.lista();
-        String monedaIncial = moneda.buscarNombreMoneda(tipo, inicial);
-        String monedaFinal = moneda.buscarNombreMoneda(tipo, destino);
+            modeloMoneda resultado = conexion.cotizar(inicial, destino, cantidad);
 
-        System.out.println("La convercion de " + cantidad + " " + monedaIncial +  " son " + resultado.conversion_rate() + " " + monedaFinal);
-        System.out.println("TRANSACCION EXITOSA");
+            NombreMoneda tipo = conexion.lista();
+            String monedaIncial = moneda.buscarNombreMoneda(tipo, inicial);
+            String monedaFinal = moneda.buscarNombreMoneda(tipo, destino);
 
+            System.out.println("La convercion de " + cantidad + " " + monedaIncial + " son " + resultado.conversion_rate() + " " + monedaFinal);
+            System.out.println("TRANSACCION EXITOSA");
+            System.out.println("""
+                    Desea realizar otra conversion?
+                    SI / NO
+                    """);
+            String eleccion = leer.next();
+            if (eleccion.equalsIgnoreCase("no")) {
+                salida = false;
+            }
+        } while (salida);
     }
 }
